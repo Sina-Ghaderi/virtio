8d196efc807099ab2dc48503b4551fde6a039f6d
Sina Ghaderi <sina@snix.ir>
2026-08-18 23:34:41 +0330

    add eventfd

---
+++ eventfd/event_fd.go
@@ -0,0 +1,73 @@
+package eventfd
+
+import (
+	"fmt"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+type EventFD struct {
+	fd int
+}
+
+// Create initializes a new eventfd.
+func Create() (*EventFD, error) {
+	fd, _, err := unix.RawSyscall(
+		unix.SYS_EVENTFD2, 0, unix.O_CLOEXEC, 0)
+
+	if err != 0 {
+		return nil, fmt.Errorf("create eventfd: %v", err)
+	}
+
+	return &EventFD{fd: int(fd)}, nil
+}
+
+// Notify alerts other users of the eventfd by writing a value of 1.
+func (e *EventFD) Notify() error {
+	val := uint64(1)
+	buf := (*[8]byte)(unsafe.Pointer(&val))[:]
+
+	for {
+		n, err := unix.Write(e.fd, buf)
+		if err == unix.EINTR {
+			continue
+		}
+		if err != nil {
+			return fmt.Errorf("write to eventfd: %v", err)
+		}
+		if n != 8 {
+			return fmt.Errorf("short write to eventfd: got %d bytes, wanted 8", n)
+		}
+		return nil
+	}
+}
+
+// Wait blocks until the eventfd is non-zero (i.e. someone calls Notify).
+func (e *EventFD) Wait() error {
+	var buf [8]byte
+
+	for {
+		n, err := unix.Read(e.fd, buf[:])
+		if err == unix.EINTR {
+			continue
+		}
+		if err != nil {
+			return fmt.Errorf("read from eventfd: %v", err)
+		}
+		if n != 8 {
+			return fmt.Errorf("short read from eventfd: got %d bytes, wanted 8", n)
+		}
+		return nil
+	}
+}
+
+// Close closes the eventfd.
+func (e *EventFD) Close() error {
+	return unix.Close(e.fd)
+}
+
+// Fd returns the underlying file descriptor if you ever need to pass it to poll/epoll.
+func (e *EventFD) Fd() int {
+	return e.fd
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/sina-ghaderi/virtio
 go 1.26.5
 
 require (
-	github.com/sina-ghaderi/tcpip v0.0.0-20260818060130-fe4c6ee7e5bd
+	github.com/sina-ghaderi/tcpip v1.0.0
 	golang.org/x/sys v0.47.0
 )
